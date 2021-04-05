b5cfa6c050a10c84b583e693a9ddec09057099ce
Jake Boyles <jakeboyles@Jakes-MacBook-Pro-2.local>
2021-04-05 16:30:40 -0400

    adding jf

---
+++ app.js
@@ -0,0 +1,8 @@
+const showModal = ()=>{
+        const modal = document.getElementById('modal')
+        modal.classList.toggle("hidden");
+
+        const skrim = document.getElementById('skrim')
+        skrim.classList.toggle("hidden");
+    
+}
